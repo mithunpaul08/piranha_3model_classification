120bafc15a7021453911a4ed7de569d3e98cd5cb
Mitch Mithun <mitch@D21ML-MMITHUN.lan>
2022-12-16 15:33:44 -0800

    LEARNING_RATE = 1e-05

--- configs.py
+++ configs.py
@@ -23,7 +23,7 @@ TRAIN_BATCH_SIZE = 8
 VALID_BATCH_SIZE = 4
 TESTING_BATCH_SIZE=1
 EPOCHS = 1000
-LEARNING_RATE = 1e-06
+LEARNING_RATE = 1e-05
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
 TESTING_FILE_PATH="./data/testing_data.csv"
