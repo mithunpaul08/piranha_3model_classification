2cd7d73df59e69624eb5563f95b045c0e9040fcb
Mitch Mithun <mitch@D21ML-MMITHUN.local>
2023-01-10 16:12:52 -0800

    will run on signature level label tuning fo rthe first time

--- configs.py
+++ configs.py
@@ -8,7 +8,7 @@ import spacy
 
 wandb.init(project="training_3model_piranha")
 
-TYPE_OF_LABEL="sentence" #["message","words","signature","sentence","all"]
+TYPE_OF_LABEL="signature" #["message","words","signature","sentence","all"]
 #is it training or testing. testing means will load a saved modeland test
 TYPE_OF_RUN="train" # ["train","test"]
 
