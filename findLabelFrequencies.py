2f6fee5ac3bcdeef04feb05cbbab821b4973f1d2
mithunpaul08 <mithunpaul08@gmail.com>
2023-01-19 09:10:49 -0800

    picking the best parameters of signature level model and going to run it with bert-cased

--- findLabelFrequencies.py
+++ findLabelFrequencies.py
@@ -1,18 +1,33 @@
 import json
+import hashlib
 
-path_annotated_emails= "data/combined_all_annotation_jan9th2023.jsonl"
-annotator1_name="uma"
-annotator2_name="mithun"
+
+def check_add_dict(input_dict,text):
+    if text not in input_dict.keys():
+        input_dict[text]=1
+    return input_dict
+
+unique_emails={}
+unique_spans={}
+path_annotated_emails= "data/all_ta3_emails.jsonl"
+annotator1= "uma"
+annotator2= "mithun"
 labels_count={}
+total_emails=0
 with open(path_annotated_emails, 'r') as annotated_file:
     Lines = annotated_file.readlines()
     for index, line in enumerate(Lines):
+
             annotations = json.loads(line)
             if "spans" in annotations:
                 for entry in annotations["spans"]:
                     label = entry["label"]
+                    str_entry="".join(entry)
                     if "_annotator_id" in annotations:
-                            if annotator1_name in annotations['_annotator_id']:
+                            if (annotator1 in annotations['_annotator_id'])  or(annotator2 in annotations['_annotator_id']):
+                                unique_emails = check_add_dict(unique_emails, line)
+                                unique_spans = check_add_dict(unique_spans, str_entry)
+                                total_emails+=1
                                 if label in labels_count:
                                     old_count=labels_count[label]
                                     labels_count[label]=old_count+1
@@ -20,5 +35,8 @@ with open(path_annotated_emails, 'r') as annotated_file:
                                     labels_count[label] = 1
 
 print(labels_count)
-print(len(labels_count))
+print(len(unique_emails))
+print(len(unique_spans))
+
+
 
