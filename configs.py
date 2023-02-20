8ec47c6349cada6d2edafe02d24fdd4545a2cda4
Mitch Paul Mithun <mithunpaul08@gmail.com>
2023-02-19 17:02:25 -0800

    will run end to end training on signature level labels again

--- configs.py
+++ configs.py
@@ -28,7 +28,7 @@ VALID_BATCH_SIZE = 4
 TESTING_BATCH_SIZE=1
 EPOCHS = 1000
 LEARNING_RATE = 1e-5
-tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
 TESTING_FILE_PATH="./data/testing_data.csv"
 
