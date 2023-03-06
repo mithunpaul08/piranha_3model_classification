499bbac55842b4ab44972212c2c3ee87c06de80f
Mitch Paul Mithun <mithunpaul08@gmail.com>
2023-03-06 11:58:56 -0800

    Run bert-base-cased messages threshold 0

--- configs.py
+++ configs.py
@@ -7,7 +7,7 @@ import sys
 import spacy
 
 QUIT_AFTER_DATACREATION=False
-TYPE_OF_LABEL="sentence" #["message","words","signature","sentence","all"]
+TYPE_OF_LABEL="message" #["message","words","signature","sentence","all"]
 #is it training or testing. testing means will load a saved modeland test
 TYPE_OF_RUN="train" # ["train","test"]
 DISABLE_WANDB=False
@@ -34,7 +34,7 @@ VALID_BATCH_SIZE = 4
 TESTING_BATCH_SIZE=1
 EPOCHS = 1000
 LEARNING_RATE = 1e-07
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
 
 TESTING_FILE_PATH="./data/testing_data.csv"
 
